69011a6709d22026e32c9bbe2cb90f94e04ddf50
Caden Jarrett <caden.jarrett99@gmail.com>
2022-04-01 14:08:15 -0600

    Junk: changed this code

--- lib/board.rb
+++ lib/board.rb
@@ -7,29 +7,48 @@ class Board
   end
 
   def create_board
-    board = {
-    :A => %w[. . . . . .],
-    :B => %w[. . . . . .],
-    :C => %w[. . . . . .],
-    :D => %w[. . . . . .],
-    :E => %w[. . . . . .],
-    :F => %w[. . . . . .],
-    :G => %w[. . . . . .]
+    @board = {
+    :A => %w[. . . . . . .],
+    :B => %w[. . . . . . .],
+    :C => %w[. . . . . . .],
+    :D => %w[. . . . . . .],
+    :E => %w[. . . . . . .],
+    :F => %w[. . . . . . .],
+    # :G => %w[. . . . . .]
     }
   end
 
   def print_board
-    puts "Welcome to the game of connect 4!".delete('"')
-    columns = ["ABCDEFG"].join
-    # columns.keys.each do |column|
-    puts columns.delete('"')
-    6.times do
-      puts %w[. . . . . . .].join
+    # columns = ["ABCDEFG"].join
+    # columns.each do |column|
+    #   puts columns.delete('"')
+    a = @board.map do |key, value|
+     puts "#{key}, #{value}"
     end
-    require "pry";binding.pry
   end
 end
+# board = Board.new()
+# puts board.print_board
+
+    # require "pry";binding.pry
+
+
     # board = []
     # @height.times do
     #   board << Array.new(@column)
     #   # board << Array.new(column{Array.new(height)})
+
+    # puts "Welcome to the game of connect 4!".delete('"')
+    # columns = ["ABCDEFG"].join
+    # puts columns.delete('"')
+    # 6.times do
+    #   p %w[. . . . . . .].join
+    # end
+    # require 'pry':binding.pry
+    # return
+
+    # final_board= []
+    # @board.values.transpose.each do |array|
+    #
+    #
+    # require 'pry':binding.pry
