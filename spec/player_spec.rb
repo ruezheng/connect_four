1a8c07f22e1107e4ef2cf27c37cb45ed8e007b90
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-02 01:40:01 -0600

    Feat: add Player class and test to show Player exists

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,25 +1,41 @@
 require 'rspec'
 require 'pry'
+require './lib/board'
 require './lib/player'
 
 RSpec.describe Player do
 
-  before :each do
-    @human_player = Player.new('X')
-    @computer_player = Player.new('O')
-  end
+  # before :each do
+  #   @player = Player.new('X')
+  #   @computer = Player.new('O')
+  # end
 
   it "exists" do
-    expect(@player).to be_an_instance_of(Player)
-    expect(@computer).to be_an_instance_of(Player)
+    @player = Player.new('X')
+    @computer = Player.new('O')
+
+    expect(player).to be_an_instance_of(Player)
+    expect(computer).to be_an_instance_of(Player)
   end
 
-  it "has a winner" do
-    expect(@player.winner?).to eq(@true)
-  end
-
-  it "has a winner" do
-    expect(@player.winner?).to eq(@false)
-  end
-
-end
+#   it "has a piece assigned to player and computer" do
+#     @player = Player.new('X')
+#     @computer = Player.new('O')
+#
+#     expect(player.)
+#   end
+#
+#   xit "has a winner or is a draw" do
+#
+#
+#     expect(@player.winner?).to eq(@true)
+#
+#
+#     expect(@computer.winner?).to eq(@false)
+#   end
+#
+#   xit "" do
+#     expect(@player.winner?).to eq(@false)
+#   end
+#
+# end
