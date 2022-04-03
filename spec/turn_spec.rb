ce1c6471bf11208bd74d4b1b908e4eafc93a9a9c
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-02 20:17:06 -0600

    Fix: bugs in turn.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -14,9 +14,22 @@ RSpec.describe Turn do
     expect(@turn).to be_an_instance_of(Turn)
   end
 
-  it "can take user input" do
+  it "can take player input" do
+    input = @turn.player_input # we put this here because when we put input in 'before:each', it asks for player input twice instead of once
+
+    expect(input).to eq("A")
+  end
+
+  it "can replace availble spot with player's piece " do
     input = @turn.player_input
 
-    expect(input).to eq("X")
+    expect().to eq("A")
   end
+
+  it "can check in all columns respond to player input" do
+    input = @turn.player_input
+
+    expect()
+  end
+
 end
