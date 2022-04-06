d59dd093009c8bd4c362d2fb62a0525de5f20d00
Caden Jarrett <caden.jarrett99@gmail.com>
2022-04-06 14:32:05 -0600

    Fix: cleaned up each file for readablitiy, deleted player.rb as there was no code in it

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,22 +4,5 @@ require './lib/turn'
 require './lib/game'
 
 puts "Welcome to Connect Four! Enter p to play, then enter a letter A-G to place a piece. Enter q to quit.".delete("")
-
-# start_value = gets.chomp
 game = Game.new()
-
 game.start_game
-
-
-
-
-
-  # game.has_won?
-  # turn = Turn.new(board)
-  # turn.computer_place_piece
-
-# game = Game.new
-
-# game.start
-
-# press q to quit game
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -20,34 +20,34 @@ RSpec.describe Turn do
     expect(@board.board[:row1][0]).to eq "X"
   end
 
-  xit "will return only letters A-G" do
+  it "will return only letters A-G" do
+
     expect(@turn.computer_input).to eq("A").or eq("B").or eq("C").or eq("D").or eq("E").or eq("F").or eq("G")
   end
 
   xit "computer can place a piece 'O' in available spot" do # this test require hardcoded computer input in order to place a piece
     @turn.computer_piece("G") # if you would like to test, make all of line 278 runnable code, and # out lines 279 and 280
-    # binding.pry
+
     expect(@board.board[:row1][6]).to eq "O"
   end
 
   it "can determine a players horizontal win" do
-    @player_win = false
-    @computer_win = false
     @turn.player_piece("C")
     @turn.player_piece("D")
     @turn.player_piece("E")
     @turn.player_piece("F")
     @turn.player_horizontal_win
-    # binding.pry
+    binding.pry
     expect(@turn.player_horizontal_win).to eq true
   end
 
-  xit "can determine a computer win" do # similar to our test before, in order to test CPU input we must hard code the input.
+  xit "can determine a computer horizontal win" do # similar to our test before, in order to test CPU input we must hard code the input.
     @turn.computer_piece("A")# to do so you must un # the (input) on line 288
     @turn.computer_piece("B")# once doing this # out the computer_input method or it will be random
     @turn.computer_piece("C")# using the # then
     @turn.computer_piece("D")
     @turn.computer_horizontal_win
+
     expect(@turn.computer_horizontal_win).to eq true
   end
 end
